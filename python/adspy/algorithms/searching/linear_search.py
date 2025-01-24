d1cd833ab3cb4b7ab5beefb809daae7ce0184c07
stankudrow <stankudrow@reply.no>
2025-01-24 16:12:48 +0300

    add the linear_search algorithm

--- python/adspy/algorithms/searching/linear_search.py
+++ python/adspy/algorithms/searching/linear_search.py
@@ -1,6 +1,7 @@
 from collections.abc import Iterable
 from typing import Any
 
+
 def linear_search(it: Iterable, value: Any) -> None | int:
     for idx, item in enumerate(it):
         if item == value:
