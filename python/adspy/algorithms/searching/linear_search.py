6cb4d878b2a24f6ce8e1ea08f91c087e5cfef83d
stankudrow <stankudrow@reply.no>
2025-01-25 15:12:33 +0300

    add docs for linear search

--- python/adspy/algorithms/searching/linear_search.py
+++ python/adspy/algorithms/searching/linear_search.py
@@ -1,9 +1,30 @@
+"""The "Linear search" algorithm.
+
+References:
+
+- https://en.wikipedia.org/wiki/Linear_search
+"""
+
 from collections.abc import Iterable
 from typing import Any
 
 
 def linear_search(it: Iterable, value: Any) -> None | int:
-    for idx, item in enumerate(it):
+    """Returns the index of the first occurence of the `value`.
+
+    If None, then the `value` was not found in the `it`erable.
+
+    Parameters
+    ----------
+    it : Iterable
+    value : Any
+
+    Returns
+    -------
+    None | int
+    """
+
+    for idx, item in enumerate(tuple(it)):
         if item == value:
             return idx
     return None
