b394e580885776c0881df5f9ddb15d6decd908ab
stankudrow <stankudrow@reply.no>
2025-04-17 11:09:21 +0300

    form a LinkedList MVP

--- python/adspy/data_structures/linked_lists.py
+++ python/adspy/data_structures/linked_lists.py
@@ -1,56 +1,41 @@
 """Linked lists data structures."""
 
+from collections.abc import Iterable, Iterator
 from typing import Any
 
 
-class LinkedNode:
-    def __init__(self, value: Any = None) -> None:
-        self.value = value
+class _LinkedNode:
+    """Doubly linked node."""
 
-    def __repr__(self) -> str:
-        cls_name = type(self).__name__
-        return f"{cls_name}(value={self.value})"
-
-
-class SinglyLinkedNode(LinkedNode):
-    __slots__ = ("value", "_next")
-
-    def __init__(self, value: Any = None) -> None:
-        super().__init__(value=value)
-        self._next: SinglyLinkedNode | None = None
-
-    @property
-    def next(self) -> "SinglyLinkedNode | None":
-        return self._next
-
-    @next.setter
-    def next(self, node: "SinglyLinkedNode | None") -> None:
-        self._next = node if node is None else SinglyLinkedNode(value=node)
-
-
-class DoublyLinkedNode(LinkedNode):
     __slots__ = ("value", "_prev", "_next")
 
     def __init__(self, value: Any = None) -> None:
-        super().__init__(value=value)
-        self._prev: DoublyLinkedNode | None = None
-        self._next: DoublyLinkedNode | None = None
+        self.value = value
+        self._prev: _LinkedNode | None = None
+        self._next: _LinkedNode | None = None
+
+    def __eq__(self, value: object) -> bool:
+        return bool(self.value == value)
+
+    def __repr__(self) -> str:
+        cls_name = type(self).__name__
+        return f"{cls_name}(value={self.value!r})"
 
     @property
-    def prev(self) -> "DoublyLinkedNode | None":
+    def prev(self) -> "_LinkedNode | None":
         return self._prev
 
     @prev.setter
-    def prev(self, node: "DoublyLinkedNode | None") -> None:
-        self._prev = node if node is None else DoublyLinkedNode(value=node)
+    def prev(self, node: "_LinkedNode | None") -> None:
+        self._prev = node if isinstance(node, _LinkedNode) else None
 
     @property
-    def next(self) -> "DoublyLinkedNode | None":
+    def next(self) -> "_LinkedNode | None":
         return self._next
 
     @next.setter
-    def next(self, node: "DoublyLinkedNode | None") -> None:
-        self._next = node if node is None else DoublyLinkedNode(value=node)
+    def next(self, node: "_LinkedNode | None") -> None:
+        self._next = node if isinstance(node, _LinkedNode) else None
 
 
 # Linked lists
@@ -59,11 +44,76 @@ class DoublyLinkedNode(LinkedNode):
 class LinkedListError(Exception): ...
 
 
-class SinglyLinkedList:
-    def __init__(self) -> None:
-        self._head: SinglyLinkedNode | None = None
-        self._tail: SinglyLinkedNode | None = None
+class LinkedList:
+    """Doubly linked list."""
+
+    __slots__ = ("_head", "_tail", "_length")
+
+    @classmethod
+    def from_iterable(cls, it: Iterable) -> "LinkedList":
+        return cls(it=it)
+
+    def __init__(self, it: None | Iterable = None) -> None:
+        self._head: _LinkedNode | None = None
+        self._tail: _LinkedNode | None = None
         self._length: int = 0
+
+        # Well, it's kinda unfair to use Python lists here :)
+        self.extend(it or ())
+
+    def __getitem__(self, key: int | slice) -> Any:
+        # inefficient yet simple and delegative
+        result = tuple(self)[key]
+        if isinstance(key, slice):
+            return type(self).from_iterable(it=result)
+        return result
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Iterable):
+            return tuple(self) == tuple(other)
+        return NotImplemented
+
+    def __iter__(self) -> Iterator[Any]:
+        node = self._head
+        while node:
+            yield node.value
+            node = node.next
 
     def __len__(self) -> int:
         return self._length
+
+    def __le__(self, other: object) -> bool:
+        if isinstance(other, Iterable):
+            return tuple(self) <= tuple(other)
+        return NotImplemented
+
+    def __lt__(self, other: object) -> bool:
+        if isinstance(other, Iterable):
+            return tuple(self) < tuple(other)
+        return NotImplemented
+
+    def prepend(self, value: Any) -> None:
+        node = _LinkedNode(value)
+        if self._head is None:
+            self._tail = node
+            self._head = node
+        else:
+            self._head.prev = node
+            node.next = self._head
+            self._head = node
+        self._length += 1
+
+    def append(self, value: Any) -> None:
+        node = _LinkedNode(value)
+        if self._tail is None:
+            self._tail = node
+            self._head = node
+        else:
+            self._tail.next = node
+            node.prev = self._tail
+            self._tail = node
+        self._length += 1
+
+    def extend(self, it: Iterable) -> None:
+        for item in it:
+            self.append(item)
--- python/tests/data_structures/test_linked_lists.py
+++ python/tests/data_structures/test_linked_lists.py
@@ -1,54 +1,71 @@
-from adspy.data_structures.linked_lists import (
-    DoublyLinkedNode,
-    SinglyLinkedNode,
+from collections.abc import Iterable
+
+import pytest
+
+from adspy.data_structures.linked_lists import LinkedList
+
+
+@pytest.mark.parametrize("elements", [[1], (2, 3), "456"])
+def test_prepend(elements: Iterable):
+    lst = LinkedList()
+    ans = list(elements)
+
+    for value in elements:
+        lst.prepend(value)
+
+    assert lst == reversed(ans)
+    assert len(lst) == len(ans)
+
+
+@pytest.mark.parametrize("elements", [[1], (2, 3), "456"])
+def test_append(elements: Iterable):
+    lst = LinkedList()
+    ans = list(elements)
+
+    for value in elements:
+        lst.append(value)
+
+    assert lst == elements
+    assert len(lst) == len(ans)
+
+
+@pytest.mark.parametrize("it", [[], "123"])
+def test_extend(it: Iterable):
+    lst = LinkedList()
+    seq = tuple(it)
+
+    lst.extend(it)
+
+    assert lst == it
+    assert len(lst) == len(seq)
+
+
+def test_from_iterable():
+    it = (1, "2", [3])
+
+    assert LinkedList.from_iterable(it) == LinkedList(it)
+
+
+def test_comparisons():
+    assert LinkedList([]) == LinkedList([])
+    assert LinkedList([1]) == LinkedList([1])
+
+    assert LinkedList([2]) != LinkedList([1])
+
+    assert LinkedList([2]) > LinkedList([1])
+    assert LinkedList([1]) < LinkedList([2])
+    assert LinkedList([1, 2]) < LinkedList([2])
+    assert LinkedList([2, 1]) > LinkedList([-1, 0, 1])
+
+    assert LinkedList([2, 1]) <= LinkedList([2, 1])
+    assert LinkedList([1, 2]) >= LinkedList([1, 2])
+
+
+@pytest.mark.parametrize(
+    ("it", "key"),
+    [([1], 0), ([1, 2], -1), ([3, 4, 5], slice(0, 3, 2))],
 )
+def test_get_item(it: list, key: int | slice):
+    lst = LinkedList(it=it)
 
-
-class TestSinglyLinkedNodes:
-    def test_value_attribute_accessors(self):
-        snode = SinglyLinkedNode()
-
-        assert snode.value is None
-
-        value = 42
-        snode.value = value
-
-        assert snode.value == value
-
-    def test_next_attribute_accessors(self):
-        snode = SinglyLinkedNode()
-
-        assert snode.next is None
-
-        next_node = SinglyLinkedNode(21)
-        snode.next = next_node
-
-        assert snode.value is None
-        assert snode.next.value is next_node
-
-
-class TestDoublyLinkedNodes:
-    def test_value_attribute_accessors(self):
-        dnode = DoublyLinkedNode()
-
-        assert dnode.value is None
-
-        value = 42
-        dnode.value = value
-
-        assert dnode.value == value
-
-    def test_next_attribute_accessors(self):
-        dnode = DoublyLinkedNode()
-
-        assert dnode.prev is None
-        assert dnode.next is None
-
-        prev_node = DoublyLinkedNode(21)
-        dnode.prev = prev_node
-
-        next_node = DoublyLinkedNode(42)
-        dnode.next = next_node
-
-        assert dnode.prev.value is prev_node
-        assert dnode.next.value is next_node
+    assert lst[key] == it[key]
