8eeccd0486bc305ec45e96732e07c4c779bf5d27
stankudrow <stankudrow@reply.no>
2025-05-08 09:22:51 +0300

    export is_sorted

--- python/adspy/algorithms/sorting/__init__.py
+++ python/adspy/algorithms/sorting/__init__.py
@@ -1,4 +1,5 @@
 from adspy.algorithms.sorting.bubble_sort import bubble_sort
+from adspy.algorithms.sorting.common import is_sorted
 from adspy.algorithms.sorting.insertion_sort import insertion_sort
 from adspy.algorithms.sorting.merge_sort import merge_sort
 from adspy.algorithms.sorting.quick_sort import quick_sort
@@ -7,6 +8,7 @@ from adspy.algorithms.sorting.selection_sort import selection_sort
 __all__ = [
     "bubble_sort",
     "insertion_sort",
+    "is_sorted",
     "merge_sort",
     "quick_sort",
     "selection_sort",
